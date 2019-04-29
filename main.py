6c6d57f785872de84617f42347b7ab6d07689f5b
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-29 17:14:39 +0700

    Add template inheritance

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 posts = [
     {
         'title': 'My First Post',
-        'content': 'This is the first post content!!'
+        'content': 'This is the first post content!!',
+        'author': 'Rahman Fadhil',
+        'created_at': 'January 14, 2019'
     },
     {
         'title': 'My Second Post',
-        'content': 'This is the second post content!!'
+        'content': 'This is the second post content!!',
+        'author': 'Chris Hemsworth',
+        'created_at': 'May 20, 2019'
     }
 ]
 
@@ -22,7 +26,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='Home Page')
 
 
 if __name__ == "__main__":
