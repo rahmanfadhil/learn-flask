6ad97b2e73e2e72eeee82fec0655d5633f687ee0
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-29 17:06:10 +0700

    Pass data through templates

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'title': 'My First Post',
+        'content': 'This is the first post content!!'
+    },
+    {
+        'title': 'My Second Post',
+        'content': 'This is the second post content!!'
+    }
+]
+
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About page</h1>'
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
