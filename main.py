14927a91430b8e0a12cfa7212d9b8670ae3a87bb
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-29 17:02:10 +0700

    Use template

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return '<h1>Home page</h1>'
+    return render_template('home.html')
 
 
 @app.route('/about')
